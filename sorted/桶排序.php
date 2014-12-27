4259f277c344457d96d93b16f0716cf163359f63
Jaylee <sjclijie@vip.qq.com>
2014-12-27 17:35:02 +0800

    update

---
+++ sorted/桶排序.php
@@ -0,0 +1,28 @@
+<?php
+    
+    //桶排序
+    //只能用于整数
+    
+    $arr = [7,3,8,4,0,2];
+
+    $book = [];
+
+    //初始化桶
+    for($i = 0; $i<=max($arr); $i++){
+        $book[$i] = 0;
+    }
+
+    //将对应的数放到桶里面
+    for ($i=0; $i<count($arr); $i++){
+        $book[$arr[$i]] ++;
+    }
+
+    //打印
+    for($i=0; $i<count($book); $i++){
+        for($j=1; $j<=$book[$i]; $j++){
+            echo $i.PHP_EOL;
+        }
+    }
+
+
+
